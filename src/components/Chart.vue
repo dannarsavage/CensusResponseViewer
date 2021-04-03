8518d3a07380a6ffa02b42b53df5d07fddab6006
Dan Narsavage <dannarsavage@users.noreply.github.com>
2021-04-03 09:34:35 -0600

    Got closer to making charts

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -7,8 +7,10 @@
 <script>
 import { CensusCountyReference } from "./CensusCountyReference";
 import { CensusResponseRateClient } from "./CensusResponseRateClient";
+//import { CensusResponseRateInformation } from "./CensusResponseRateInformation";
 import * as am4core from "@amcharts/amcharts4/core";
 import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 
 export default {
   name: 'Chart',
@@ -17,13 +19,15 @@ export default {
   },
   data() {
     return {
-      responseClient: null
+      chart: null,
+      responseClient: null,
+      responseInfo: null
     }
   },
   mounted() {
-    console.log(am4core);
-    console.log(am4charts);
+    am4core.useTheme(am4themes_animated);    
     this.responseClient = new CensusResponseRateClient('https://api.census.gov/data/2020/dec/responserate');
+    this.chart = am4core.create("chart", am4charts.PieChart);
   },
   watch: {
     countyReference: async function (val) {
@@ -35,13 +39,44 @@ export default {
   methods: {
     setCounty: async function (val) {
       // TODO: Get data from the census for the county and use it to update the chart
-      const thing = this.responseClient.getResponseRateForCountyAndState(val);
-      console.log(thing);
-    }
+      this.responseInfo = await this.responseClient.getResponseRateForCountyAndState(val);
+      await this.updateChartWithData(val);
+      //console.log(`${this.responseInfo.Name}: ${this.responseInfo.CumulativeTotalResponseRate}`);
+    },
+    updateChartWithData: async function(data) {
+      const totalResponseRate = data.CumulativeTotalResponseRate;
+      const internetResponseRate = data.CumulativeInternetResponseRate;
+      const noResponseRate = 100 - totalResponseRate;
+      const mailedResponseRate = totalResponseRate - internetResponseRate;
+
+      this.chart.data = [{
+        "responseType": "No Response",
+        "responseRate": noResponseRate
+      }, {
+        "responseType": "Mailed",
+        "responseRate": mailedResponseRate
+      }, {
+        "responseType": "Internet",
+        "responseRate": internetResponseRate
+      }];
+      // Add and configure Series
+      let pieSeries = this.chart.series.push(new am4charts.PieSeries());
+      pieSeries.dataFields.value = "responseRate";
+      pieSeries.dataFields.category = "responseType";
+      pieSeries.slices.template.stroke = am4core.color("#fff");
+      pieSeries.slices.template.strokeOpacity = 1;
+
+      // This creates initial animation
+      pieSeries.hiddenState.properties.opacity = 1;
+      pieSeries.hiddenState.properties.endAngle = -90;
+      pieSeries.hiddenState.properties.startAngle = -90;
+
+      this.chart.hiddenState.properties.radius = am4core.percent(0); 
+   }
   },
   beforeDestroy() {
-    if (this.map) {
-      this.map.remove();
+    if (this.chart) {
+      this.chart = null;
     }
   }
 }
