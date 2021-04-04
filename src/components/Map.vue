8a1dea46643d74fc61d2fca1c6c4469c5063d27b
Dan Narsavage <dannarsavage@users.noreply.github.com>
2021-04-04 10:57:41 -0600

    Added map event handlers for click & double-click. Fix #6

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -20,7 +20,8 @@ export default {
     return {
       map: null,
       countyLayerGroup: null,
-      countyClient: null
+      countyClient: null,
+      mapDoubleClickTimer: null
     }
   },
   mounted() {
@@ -64,16 +65,41 @@ export default {
       });
       return L.polygon(latlngs, {color: 'red'});
     },
-    getCountyFromClick: async function (e) {
-      // TODO: Figure a way to isolate this to single clicks
-      // https://stackoverflow.com/questions/29035896/leaflet-dont-fire-click-event-function-on-doubleclick
-      if (e.originalEvent.altKey || e.originalEvent.ctrlKey || e.originalEvent.shiftKey) {
+    /**
+     * Handle single-click event 
+     *    - allows user to select a county by clicking on the map
+     *    - checks/sets double-click timer to prevent handling of single-click events during double-clicks
+     * https://stackoverflow.com/questions/29035896/leaflet-dont-fire-click-event-function-on-doubleclick
+     * @param   {event}      e    Event parameters containing location of click
+     */
+    clickAction: async function (e) {
+      if (this.mapDoubleClickTimer !== null) {
         return;
       }
-      const latitude = e.latlng.lat;
-      const longitude = e.latlng.lng;
-      const countyReturn = await this.countyClient.getCountyAtLatLong (latitude, longitude, true);
-      this.emitCounty(countyReturn);
+      this.mapDoubleClickTimer = setTimeout(async () => {
+
+        if (
+            !e.originalEvent.altKey 
+            && !e.originalEvent.ctrlKey 
+            && !e.originalEvent.shiftKey) 
+          {
+          const latitude = e.latlng.lat;
+          const longitude = e.latlng.lng;
+          const countyReturn = await this.countyClient.getCountyAtLatLong (latitude, longitude, true);
+          this.emitCounty(countyReturn);
+        }
+
+        this.mapDoubleClickTimer = null;
+      }, 200);
+    },
+    /**
+     * Handle double-click event - simply resets the double-click timer to prevent handling of single-click events
+     * https://stackoverflow.com/questions/29035896/leaflet-dont-fire-click-event-function-on-doubleclick
+     * @param   {event}      e    Event parameters (unused so absent, but can be replaced if needed)
+     */
+    doubleClickAction: async function () {
+      clearTimeout(this.mapDoubleClickTimer);
+      this.mapDoubleClickTimer = null;
     },
     /**
      * Emit an event that the county has been updated
@@ -92,7 +118,8 @@ export default {
           '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       }).addTo(map);
       map.setView([44,-116.5],3);
-      map.on('click', this.getCountyFromClick);
+      map.on('click', this.clickAction);
+      map.on('dblclick', this.doubleClickAction);
       return map;
     }
   },
@@ -112,7 +139,7 @@ h1 {
 .mapContainer {
   width: 50vw;
   background-color: lightskyblue;
-  padding: 10px;
+  padding: 5px;
   z-index: inherit;
 }
 #map {
